d3eab9a4b80bea8eac04568cf9e32d306248404c
Piotr Bednarski <piotr@michalbednarski.pl>
2022-04-17 16:41:05 +0200

    config

--- Program.cs
+++ Program.cs
@@ -5,12 +5,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace SubiektGT_Sfera
 {
+
     public class Program
     {
+        [STAThread]
         public static void Main(string[] args)
         {
+            
             CreateHostBuilder(args).Build().Run();
         }
 
--- Worker.cs
+++ Worker.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using ADODB;
+using System.Data.SqlClient;
+
 
 namespace SubiektGT_Sfera
 {
@@ -22,6 +25,29 @@ namespace SubiektGT_Sfera
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                try
+                {
+                    InsERT.GT gt = new InsERT.GT();
+                    gt.Produkt = InsERT.ProduktEnum.gtaProduktSubiekt;
+                    gt.Serwer = "ITPB\\INSERTGT";
+                    gt.Baza = "Projekt1";
+                    gt.Autentykacja = InsERT.AutentykacjaEnum.gtaAutentykacjaMieszana;
+                    gt.Uzytkownik = "sa";
+                    gt.UzytkownikHaslo = "";
+                    gt.Operator = "Szef";
+                    gt.OperatorHaslo = "";
+
+                    InsERT.Subiekt subiekt = (InsERT.Subiekt)gt.Uruchom((Int32)InsERT.UruchomDopasujEnum.gtaUruchomDopasuj, (Int32)InsERT.UruchomEnum.gtaUruchomNieArchiwizujPrzyZamykaniu);
+
+
+
+                    subiekt.Okno.Widoczne = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
                 await Task.Delay(1000, stoppingToken);
             }
         }
